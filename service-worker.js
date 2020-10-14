f3a9fe6b0529b373e6876d579b3c3afd0c07eefe
vachara010 <vachara449@hotmail.com>
2020-10-15 04:25:24 +0700

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.4eb8a24ff42a9b03a0333685c5b57613.js"
+  "/portfolio/precache-manifest.9a776bcb4c406034e7b9ad6e20306ae7.js"
 );
 
 self.addEventListener('message', (event) => {
